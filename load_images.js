7f22e288621e1573d92ff9b25a2798d6196287b9
Kelly Milligan <kelly@milligan.net.nz>
2017-06-29 10:50:47 +0100

    Add simple image loading util

---
+++ load_images.js
@@ -0,0 +1,38 @@
+/*
+    Load images in the supplied manifest
+
+    image_manifest      Array       array of image meta objects
+    loadedd_callback    Function    callback called when all images are loaded
+
+    Manifest structure:
+    [
+        {
+            id: 'image-name',
+            url: 'path/to/image.png'
+        }
+    ]
+*/
+
+export default function (
+
+    image_manifest, loaded_callback
+
+) {
+
+    let loaded = [];
+
+    for ( let i = 0, len = image_manifest.length; i < len; i++ ) {
+
+        let item = image_manifest[ i ];
+        let image = document.createElement( 'img' );
+
+        image.onload = () => {
+
+            loaded.push( Object.assign( { img: image }, item ) );
+
+            if ( loaded.length === len ) { loaded_callback( loaded ); }
+        };
+
+        image.src = item.url;
+    }
+}
