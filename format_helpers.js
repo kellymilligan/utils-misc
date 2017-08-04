0b949626dd397735eb329de38127cbd1c4bcbb5b
Kelly Milligan <kelly@milligan.net.nz>
2017-08-04 14:42:04 +0100

    fix whitespace

--- format_helpers.js
+++ format_helpers.js
@@ -1,10 +1,13 @@
 /*
     Format a number value to include thousands separators (i.e. 10000 -> "10,000")
     ---
+
     value       Number      Value to format
     character   String      Character to inject
+
     ---
     Returns     String      Formatted value
+
 */
 
 export function numberToThousands(
