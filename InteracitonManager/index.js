2b852db90b98dfacfbf564556e92d3c65be33388
Kelly Milligan <kelly@milligan.nz>
2018-02-15 10:39:17 +0000

    Move InteractionManager into directory with singleton wrapper

---
+++ InteracitonManager/index.js
@@ -0,0 +1,16 @@
+/*
+  Establish the InteractionManager as a global singleton.
+
+  The main instance can be imported as a standard module:
+  import InteractionManager from 'utils/InteractionManager'
+
+  Access to the underlying class is still available if needed:
+  import { InteractionManager } from 'utils/InteractionManager'
+*/
+
+import InteractionManager from './InteractionManager';
+
+const instance = new InteractionManager();
+
+export { InteractionManager };
+export default instance;
