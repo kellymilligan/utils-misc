b04891f1b98adbec731401081fdde9f11acc14c0
Kelly Milligan <kelly@milligan.nz>
2018-03-21 15:28:46 +0000

    Add paper size constants util

---
+++ paper.js
@@ -0,0 +1,113 @@
+export default {
+  resolution: 11.81, // 300dpi = 300px / 25.4mm = 11.81px per mm
+  A0: {
+    mm: {
+      width: 841,
+      height: 1189,
+    },
+    px: {
+      width: 9933,
+      height: 14043,
+    }
+  },
+  A1: {
+    mm: {
+      width: 594,
+      height: 841,
+    },
+    px: {
+      width: 7016,
+      height: 9933,
+    }
+  },
+  A2: {
+    mm: {
+      width: 420,
+      height: 594,
+    },
+    px: {
+      width: 4961,
+      height: 7016,
+    }
+  },
+  A3: {
+    mm: {
+      width: 297,
+      height: 420,
+    },
+    px: {
+      width: 3508,
+      height: 4961,
+    }
+  },
+  A4: {
+    mm: {
+      width: 210,
+      height: 297,
+    },
+    px: {
+      width: 2480,
+      height: 3508,
+    }
+  },
+  A5: {
+    mm: {
+      width: 148,
+      height: 210,
+    },
+    px: {
+      width: 1748,
+      height: 2480,
+    }
+  },
+  A6: {
+    mm: {
+      width: 105,
+      height: 148,
+    },
+    px: {
+      width: 1240,
+      height: 1748,
+    }
+  },
+  A7: {
+    mm: {
+      width: 74,
+      height: 105,
+    },
+    px: {
+      width: 874,
+      height: 1240,
+    }
+  },
+  A8: {
+    mm: {
+      width: 52,
+      height: 74,
+    },
+    px: {
+      width: 614,
+      height: 874,
+    }
+  },
+  A9: {
+    mm: {
+      width: 37,
+      height: 52,
+    },
+    px: {
+      width: 437,
+      height: 614,
+    }
+  },
+  A10: {
+    mm: {
+      width: 26,
+      height: 37,
+    },
+    px: {
+      width: 307,
+      height: 437,
+    }
+  },
+}
