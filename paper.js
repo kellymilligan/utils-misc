89e0abfa07572a43a8f4264aff01ef8808e20934
Kelly Milligan <kelly@milligan.nz>
2018-03-22 16:31:07 +0000

    Add additional paper sizes to paper.js

--- paper.js
+++ paper.js
@@ -1,6 +1,29 @@
 export default {
   resolution: 11.81, // 300dpi = 300px / 25.4mm = 11.81px per mm
-  A0: {
+  list: [ 'B0', 'A0', 'B1', 'A1', 'B2', 'A2', 'B3', 'A3', 'B4', 'A4', 'B5', 'A5' ],
+  // A sizes
+  // https://papersizes.io/a/
+  '4A0': {
+    mm: {
+      width: 1682,
+      height: 2378,
+    },
+    px: {
+      width: 19866,
+      height: 28087,
+    }
+  },
+  '2A0': {
+    mm: {
+      width: 1189,
+      height: 1682,
+    },
+    px: {
+      width: 14043,
+      height: 19866,
+    }
+  },
+  'A0': {
     mm: {
       width: 841,
       height: 1189,
@@ -10,7 +33,7 @@ export default {
       height: 14043,
     }
   },
-  A1: {
+  'A1': {
     mm: {
       width: 594,
       height: 841,
@@ -20,7 +43,7 @@ export default {
       height: 9933,
     }
   },
-  A2: {
+  'A2': {
     mm: {
       width: 420,
       height: 594,
@@ -30,7 +53,7 @@ export default {
       height: 7016,
     }
   },
-  A3: {
+  'A3': {
     mm: {
       width: 297,
       height: 420,
@@ -40,7 +63,7 @@ export default {
       height: 4961,
     }
   },
-  A4: {
+  'A4': {
     mm: {
       width: 210,
       height: 297,
@@ -50,7 +73,7 @@ export default {
       height: 3508,
     }
   },
-  A5: {
+  'A5': {
     mm: {
       width: 148,
       height: 210,
@@ -60,7 +83,7 @@ export default {
       height: 2480,
     }
   },
-  A6: {
+  'A6': {
     mm: {
       width: 105,
       height: 148,
@@ -70,7 +93,7 @@ export default {
       height: 1748,
     }
   },
-  A7: {
+  'A7': {
     mm: {
       width: 74,
       height: 105,
@@ -80,7 +103,7 @@ export default {
       height: 1240,
     }
   },
-  A8: {
+  'A8': {
     mm: {
       width: 52,
       height: 74,
@@ -90,7 +113,7 @@ export default {
       height: 874,
     }
   },
-  A9: {
+  'A9': {
     mm: {
       width: 37,
       height: 52,
@@ -100,7 +123,7 @@ export default {
       height: 614,
     }
   },
-  A10: {
+  'A10': {
     mm: {
       width: 26,
       height: 37,
@@ -108,6 +131,78 @@ export default {
     px: {
       width: 307,
       height: 437,
+    }
+  },
+  // B sizes
+  // https://papersizes.io/b/
+  'B0': {
+    mm: {
+      width: 1000,
+      height: 1414,
+    },
+    px: {
+      width: 11811,
+      height: 16701,
+    }
+  },
+  'B1': {
+    mm: {
+      width: 707,
+      height: 1000,
+    },
+    px: {
+      width: 8350,
+      height: 11811,
+    }
+  },
+  'B2': {
+    mm: {
+      width: 500,
+      height: 707,
+    },
+    px: {
+      width: 5906,
+      height: 8350,
+    }
+  },
+  'B3': {
+    mm: {
+      width: 353,
+      height: 500,
+    },
+    px: {
+      width: 4169,
+      height: 5906,
+    }
+  },
+  'B4': {
+    mm: {
+      width: 250,
+      height: 353,
+    },
+    px: {
+      width: 2953,
+      height: 4169,
+    }
+  },
+  'B5': {
+    mm: {
+      width: 176,
+      height: 250,
+    },
+    px: {
+      width: 2079,
+      height: 2953,
+    }
+  },
+  'B6': {
+    mm: {
+      width: 125,
+      height: 176,
+    },
+    px: {
+      width: 1476,
+      height: 2079,
     }
   },
 }
